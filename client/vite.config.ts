6817eb3c88d4c8204abd00be297e5f47041c9454
ZackAmes <zames6584@gmail.com>
2025-10-27 14:20:46 -0400

    add server to vite conf

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,4 +13,11 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        allowedHosts: [process.env['DEPLOY_NAME'] + '.ponzis.fun'],
+
+    },
 });
