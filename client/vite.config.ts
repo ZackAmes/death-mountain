17345f2205c8be1380a2ea75841882ef1de1a664
ZackAmes <zames6584@gmail.com>
2025-10-27 14:32:20 -0400

    remove top level await

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,12 +2,15 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
-import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), wasm(), topLevelAwait(), mkcert()],
+    plugins: [react(), wasm(), mkcert()],
+    worker: {
+        format: 'es',
+        plugins: () => [wasm()],
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
